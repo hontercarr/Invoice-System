9d8782715e060c4d6e2b0adb219d113f4f42697f
suaw <suawdev@gmail.com>
2019-02-07 18:16:12 -0600

    Update 0.0.4

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(3000, () => {
-    console.log('Express server started at port : 3000');
+var port = process.env.PORT || 80;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
 
 // Auto Emails
