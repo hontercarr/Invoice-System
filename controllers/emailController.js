0a6c3434898dae63f5ca39f0a837eb202f127df8
suaw <suawdev@gmail.com>
2019-03-18 20:00:35 -0500

    Updates for mass emailing

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -4,122 +4,81 @@ const mongoose = require("mongoose");
 const Customer = mongoose.model("Customer");
 const Invoice = mongoose.model("Invoice");
 
-var hbs = require('nodemailer-express-handlebars');
+var hbs = require("nodemailer-express-handlebars");
 //attach the plugin to the nodemailer transporter
 
+// router.get("/email/:email", (req, res) => {
+//   let emailtest = req.params;
+//   Customer.distinct("email", function(error, ids).then(customers => {
+//     let xdmydood = customers[0].fullName;
+//     Invoice.find({ invoice_customer: xdmydood }).then(thecustomer => {
+//       sendInvoice(emailtest.email, thecustomer);
+//     });
+//   });
+//   res.render("email/email");
+// });
 
-router.get("/email/:email", (req, res) => {
-  // res.render("email/email", {
-  //   viewTitle: "Email"
-  // });
-  let emailtest = req.params;
-  // Debug - Print Email
-  // console.log(emailtest.email);
-  // sendInvoice(emailtest)
-  // sendInvoice(emailtest.email)
-  // res.render('email/email')
-  Customer.find({email: emailtest.email})
-    .then(customers => {
-      // Debug - Print clients name
-      // console.log(customers[0].fullName);
-      let xdmydood = customers[0].fullName;
-  Invoice.find({invoice_customer: xdmydood})
-    .then(thecustomer => {
-      // Debug - Print the customer's entire invoice array
-      // console.log(thecustomer);
-      sendInvoice(emailtest.email, thecustomer);
-    })
-    })
-    res.render('email/email');
-  
+router.get("/all", (req, res) => {
+  Customer.distinct("email", function(error, ids) {
+    const theEmail = ids[0];
+    console.log(theEmail);
+    Customer.find({ email: theEmail }).then(customers => {
+      const customerName = customers[0].fullName;
+      Invoice.find({ invoice_customer: customerName }).then(thecustomer => {
+        console.log(thecustomer);
+        // sendInvoice(ids, thecustomer);
+      });
+      res.render("email/email");
+    });
+  });
 });
 
+// router.get("/all", (req, res) => {
+//   Customer.distinct("email", function(error, ids) {
+//     sendInvoice(ids);
+//   });
+// });
 
-function sendInvoice(emailu, data) {
-  var nodemailer = require('nodemailer');
-  var hbs = require('nodemailer-express-handlebars');
+function sendInvoice(ids, thecustomer) {
+  var nodemailer = require("nodemailer");
+  var hbs = require("nodemailer-express-handlebars");
   var options = {
-     viewEngine: {
-         extname: '.hbs',
-         layoutsDir: 'views/email/',
-         defaultLayout : 'template',
-         partialsDir : 'views/partials/'
-     },
-     viewPath: 'views/email/',
-     extName: '.hbs'
- };
+    viewEngine: {
+      extname: ".hbs",
+      layoutsDir: "views/email/",
+      defaultLayout: "template",
+      partialsDir: "views/partials/"
+    },
+    viewPath: "views/email/",
+    extName: ".hbs"
+  };
 
-  
   var transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      user: 'retardretard750@gmail.com',
-      pass: 'retardretard123'
+      user: "retardretard750@gmail.com",
+      pass: "retardretard123"
     }
   });
-  // transporter.use('compile', hbs(email.hbs));
 
-  // var mailOptions = {
-  //   from: 'retardretard750@gmail.com',
-  //   to: emailu,
-  //   bcc: '',
-  //   subject: 'Test',
-  //   html: ''
-  //   // attachments: [{
-  //   //   filename: 'ctivetrips.png',
-  //   //   path: 'http://localhost/img/activetrips.png'
-  //   // }]
-  // };
-
-//   var mailOptions = {
-//     from: 'retardretard750@gmail.com',
-//     to: emailu,
-//     subject: 'Test :)',
-//     template: 'email',
-//     context: {
-//         name: 'Name'
-//     }
-//  }
-  transporter.use('compile', hbs(options));
+  transporter.use("compile", hbs(options));
   transporter.sendMail({
-    form: 'retardretard750@gmail.com',
-    to: emailu,
-    subject: 'CattyShack Invoices',
-    template: 'email_body',
+    form: "retardretard750@gmail.com",
+    to: ids,
+    subject: "CattyShack Invoices",
+    template: "email_body",
     context: {
-      data: data
+      thecustomer: thecustomer
     }
   });
 }
 
 router.get("/test", (req, res) => {
-  Customer.find({})
-    .then(customers => {
-      res.render('email/test', {
-        customers: customers
-      });
+  Customer.find({}).then(customers => {
+    res.render("email/test", {
+      customers: customers
     });
+  });
 });
 
-function sendAll() {
-  Customer.find({})
-    .then(all => {
-      i = 0;
-      if( i > 0) {
-        console.log(all.email[i]);
-        i++;
-      } else {
-        return 0;
-      }
-        })
-}
-console.log(sendAll());
-
 module.exports = router;
-
-
-
-
-
-
-
